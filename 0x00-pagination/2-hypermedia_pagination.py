9381d2d702c75772c7a875eb7535a9577a369543
8srael <kouassiisrael36@gmail.com>
2023-08-20 22:50:43 +0000

    Task 2. hypermedia pagination✅

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""Task 1. Simple pagination"""
+"""Task 2. Hypermedia pagination"""
 
 import csv
+import math
 from typing import List
 index_range = __import__('0-simple_helper_function').index_range
 
@@ -30,3 +31,23 @@ class Server:
         assert type(page_size) is int and page_size > 0
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Gets the page from the dataset with hypermedia pagination."""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        start, end = index_range(page, page_size)
+        current_data = self.dataset()[start:end]
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+
+        return {
+            'page_size' : page_size,
+            'page' : page,
+            'data': current_data,
+            'next_page' : next_page,
+            'prev_page' : prev_page,
+            'total_pages' : total_pages
+        }
+
