52e2951884359136d0f25b72db4a156b6f5b7cf6
8srael <kouassiisrael36@gmail.com>
2023-08-29 19:09:58 +0000

    Task 0. basic flask app🆗

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" Task 0. Basic Flask app"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns a template"""
+    return render_template('0-index.html')
