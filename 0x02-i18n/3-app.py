6178d0a107983c84dd8d9cc3b548a3878f2493e8
8srael <kouassiisrael36@gmail.com>
2023-08-29 21:26:08 +0000

    Task 3 files✅

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+""" Task 3. Parameterize templates """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Available languages configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns a template"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
