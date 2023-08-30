bb3e2c71fd25186e3a5a372ec5f9f6f9c2e73335
8srael <kouassiisrael36@gmail.com>
2023-08-30 00:24:41 +0000

    Task 6 files

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -59,7 +59,7 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def index():
     """returns a template"""
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == "__main__":
