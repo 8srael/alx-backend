9935553ba751aae55daad201d5c416e61b7a0291
8srael <kouassiisrael36@gmail.com>
2023-09-27 21:48:31 +0000

    Task 10. job creation function writed✅

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,29 @@
+/**
+ * Task 10. Writing the job creation function
+ */
+
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) throw Error('Jobs is not an array');
+
+  for (const jobData of jobs) {
+    const job = queue.create('push_notification_code_3', jobData);
+
+    job
+      .on('enqueue', () => {
+        console.log(`Notification job created: ${job.id}`);
+      })
+      .on('complete', () => {
+        console.log(`Notification job ${job.id} completed`);
+      })
+      .on('progress', (progress) => {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      })
+      .on('failed', (erorMsg) => {
+        console.log(`Notification job ${job.id} failed: ${erorMsg}`);
+      });
+
+    job.save();
+  }
+};
+
+export default createPushNotificationsJobs;
